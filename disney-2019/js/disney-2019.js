a35f0145f6e10429543f3857a3d027522231213c
Michael Fazio <mfazio@skylinetechnologies.com>
2018-12-12 22:19:27 -0600

    Add ability to switch background for Disney 2019 timer.

--- disney-2019/js/disney-2019.js
+++ disney-2019/js/disney-2019.js
@@ -1,53 +1,86 @@
 const DateTime = luxon.DateTime;
 const startTime = DateTime.local(2018, 7, 22, 16, 40);
 
-const timers = {
-    /*bookDining: {
-        divId: 'book-dining-div',
-        endDate: DateTime.local(2018, 7, 27, 6, 0, 0)
-    },*/
-    bookFastpass: {
-        divId: 'book-fastpass-div',
-        endDate: DateTime.local(2018, 11, 24, 6, 0, 0)
-    },
-    checkIn: {
-        divId: 'check-in-div',
-        endDate: DateTime.local(2019, 1, 23, 6, 25, 0)
-    }
-};
+const defaultBarColor = "#E7F7F5";
+const defaultTrackColor = "255, 255, 255";
 
-$.each(timers, function (timerId, timer) {
-    $('#' + timer.divId + ' .chart').each(function (ind, chart) {
-        $(chart).easyPieChart({
-            scaleColor: false,
-            trackColor: 'rgba(255,255,255,0.3)',
-            barColor: '#E7F7F5',
-            lineWidth: 6,
-            lineCap: 'butt',
-            size: 95
+const images = [
+    {
+        "imageName": "9322026dbcf95e28b6f49b97e5afce46.jpg",
+        "directoryName": "1440x2560"
+    },
+    {
+        "imageName": "fh9z9tbojonz.png",
+        "directoryName": "1440x2560"
+    },
+    {
+        "imageName": "160f9f9f3eff6c613ad2b5f980e4913e.jpg",
+        "directoryName": "1920x1080"
+    },
+    {
+        "imageName": "royal-holiday-hotel-resort-the-rosen-centre-hotel-estados-unidos-florida-orlando.jpg",
+        "directoryName": "1920x1080",
+        "barColor": "#263749",
+        "trackColor": "38, 55, 73"
+    },
+    {
+        "imageName": "videoblocks-monorail-disney-world-park-epcot-orlando-usa_rzncodv0x_thumbnail-full09.png",
+        "directoryName": "1920x1080",
+        "barColor": "#4EFCDB"
+    },
+    {
+        "imageName": "videoblocks-sunny-day-in-walt-disney-world-magic-kingdom-orlando-usa_rztcz-8cg_thumbnail-full11.png",
+        "directoryName": "1920x1080",
+        "barColor": "#294B84",
+        "trackColor": "41, 75, 132"
+    },
+    {
+        "imageName": "walt-disney-world-six-parks-uk-video-loop.jpg",
+        "directoryName": "1920x1080"
+    }
+];
+
+function configureTimers(barColor, trackColor) {
+    const timers = {
+        checkIn: {
+            divId: 'check-in-div',
+            endDate: DateTime.local(2019, 1, 23, 6, 25, 0)
+        }
+    };
+
+    $.each(timers, function (timerId, timer) {
+        $('#' + timer.divId + ' .chart').each(function (ind, chart) {
+            $(chart).easyPieChart({
+                scaleColor: false,
+                trackColor: `rgba(${trackColor || defaultTrackColor},0.3)`,
+                barColor: barColor || defaultBarColor,
+                lineWidth: 6,
+                lineCap: 'butt',
+                size: 95
+            });
         });
     });
-});
 
-setInterval(function (a, b, c) {
-    $.each(timers, function (timerId, timer) {
-        const diff = timer.endDate.diffNow(['days', 'hours', 'minutes', 'seconds']).toObject();
-        const startDiff = timer.endDate.diff(startTime, ['days']).toObject();
-        const diffs = {
-            days: Math.round(100 * ((startDiff.days - diff.days) / startDiff.days)),
-            hours: Math.round(100 * ((24 - diff.hours) / 24)),
-            minutes: Math.round(100 * ((60 - diff.minutes) / 60)),
-            seconds: Math.round(100 * ((60 - diff.seconds) / 60))
-        };
-        const values = {
-            days: Math.round(diff.days),
-            hours: Math.round(diff.hours),
-            minutes: Math.round(diff.minutes),
-            seconds: Math.round(diff.seconds)
-        };
-        updateCharts(timer.divId, diffs, values);
-    });
-}, 750);
+    setInterval(function (a, b, c) {
+        $.each(timers, function (timerId, timer) {
+            const diff = timer.endDate.diffNow(['days', 'hours', 'minutes', 'seconds']).toObject();
+            const startDiff = timer.endDate.diff(startTime, ['days']).toObject();
+            const diffs = {
+                days: Math.round(100 * ((startDiff.days - diff.days) / startDiff.days)),
+                hours: Math.round(100 * ((24 - diff.hours) / 24)),
+                minutes: Math.round(100 * ((60 - diff.minutes) / 60)),
+                seconds: Math.round(100 * ((60 - diff.seconds) / 60))
+            };
+            const values = {
+                days: Math.round(diff.days),
+                hours: Math.round(diff.hours),
+                minutes: Math.round(diff.minutes),
+                seconds: Math.round(diff.seconds)
+            };
+            updateCharts(timer.divId, diffs, values);
+        });
+    }, 750);
+}
 
 function updateCharts(divId, diffs, values) {
     updateChart(divId, 'days', diffs.days, values.days);
@@ -60,3 +93,20 @@ function updateChart(divId, chartName, diff, value) {
     $('#' + divId + ' .' + chartName + '-chart').data('easyPieChart').update(diff || 100);
     $('#' + divId + ' .' + chartName + '-value').text(value || 0);
 }
+
+const $doc = $(document);
+$doc.ready(() => {
+    const validImages = images.filter((image) => image.directoryName === ($doc.width() > $doc.height() ? '1920x1080' : '1440x2560'));
+
+    const randomImage = validImages[parseInt(Math.random() * (validImages.length))];
+
+    console.log("Image", randomImage);
+
+    const imagePath = `img/backgrounds/${randomImage.directoryName}/${randomImage.imageName}`;
+    $("#check-in-div").css('background-image', `url(${imagePath})`);
+
+    $('h4, body').css('color', `rgb(${randomImage.trackColor || defaultTrackColor})`);
+
+    configureTimers(randomImage.barColor, randomImage.trackColor);
+
+});
